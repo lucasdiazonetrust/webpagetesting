38b7acb0333d5fa93fbfad6860ddd8d7cc52dc6f
lucasdiazonetrust <lucas.diaz@onetrust.com>
2026-09-10 11:19:03 +0200

    Update telemetryscript.js

--- scripts/telemetryscript.js
+++ scripts/telemetryscript.js
@@ -1,1 +1,63 @@
 
+
+function installCookie() {
+	const expiryDate = new Date();
+	expiryDate.setMonth(expiryDate.getMonth() + 1);
+	document.cookie = "testingCookie_telemetry=22222; expires="+expiryDate.toUTCString()+"; path=/; SameSite=Lax";
+	console.log("Cookie testingCookieTelemetry installed successfully!");
+	alert("Cookie testingCookieTelemetry installed successfully!");
+}
+
+const EXPIRY_DATE = new Date('2026-12-31T23:59:59Z').getTime();
+
+
+ /*LOCAL STORAGE*/
+
+function setLocalStorage(key, value) {
+  const item = {
+    value,
+    expiry: EXPIRY_DATE
+  };
+  localStorage.setItem(key, JSON.stringify(item));
+  console.log(getLocalStorage('LocalStorage_telemetry'));
+  alert("localStorage installed successfully!"+getLocalStorage('LocalStorage_telemetry'));
+}
+
+function getLocalStorage(key) {
+  const itemStr = localStorage.getItem(key);
+  if (!itemStr) return null;
+
+  const item = JSON.parse(itemStr);
+
+  if (Date.now() > item.expiry) {
+    localStorage.removeItem(key);
+    return null;
+  }
+
+  return item.value;
+}
+
+/*SESSION STORAGE*/
+function setSessionStorage(key, value) {
+  const item = {
+    value,
+    expiry: EXPIRY_DATE
+  };
+  sessionStorage.setItem(key, JSON.stringify(item));
+  console.log(getSessionStorage('SessionStorage_telemetry'));
+  alert("sessionStorage installed successfully!"+getSessionStorage('SessionStorage_telemetry'));
+}
+
+function getSessionStorage(key) {
+  const itemStr = sessionStorage.getItem(key);
+  if (!itemStr) return null;
+
+  const item = JSON.parse(itemStr);
+
+  if (Date.now() > item.expiry) {
+    sessionStorage.removeItem(key);
+    return null;
+  }
+
+  return item.value;
+}
